f442131461430225078afad8af81e3fe297aa679
JoaKox5 <joaquinparraga51@gmail.com>
2023-10-20 08:11:19 -0300

    templates

--- app/views.py
+++ app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 # Create your views here.
-def Home(request):
-    return HttpResponse("SAC: Laboratorio Quimico") 
+def home(request):
+    return render(request, "home.html")
+
+def test(request):
+    return HttpResponse("<h1>Estas en Log In :)</h1>")  # Corregir esta línea
