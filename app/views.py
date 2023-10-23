cee1d013d6a72664443119fb2bcd2264e9f11c90
HermesPuig <hermespuig10@gmail.com>
2023-10-23 08:20:54 -0300

    cmabio

--- app/views.py
+++ app/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home(request):
-    return render(request, "home.html")
-
+def Home(request):
+    return HttpResponse("Hola mudno desde la app") 
 def test(request):
-    return HttpResponse("<h1>Estas en Log In :)</h1>")  # Corregir esta línea
+    pass
