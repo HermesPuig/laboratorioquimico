75e417959077c48713007cbf40a18986b116c01d
JoaKox5 <joaquinparraga51@gmail.com>
2023-10-31 17:29:06 -0300

    update

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-<<<<<<< HEAD
+
 def home(request):
     return render(request, "home.html")
 
@@ -11,9 +11,4 @@ def login(request):
 
 def solicitud(request):
     return render(request, "solicitud.html")
-=======
-def Home(request):
-    return HttpResponse("Hola mudno desde la app") 
-def test(request):
-    pass
->>>>>>> 290732422d6930192edd4a4859000fbe7fcd3b88
+
