e23061de6e84871166fa09ffb951e14d1a3d0849
derek7018 <derek198miller@gmail.com>
2023-11-09 08:13:06 -0300

    Vinculacion con la base de datos

--- app/admin.py
+++ app/admin.py
@@ -3,3 +3,8 @@ from .models import *
 # Register your models here.
 
 
+admin.site.register(usuario)
+admin.site.register(Pedido)
+admin.site.register(receta)
+admin.site.register(mediciones)
+admin.site.register(TipoDocumento)
--- app/models.py
+++ app/models.py
@@ -3,10 +3,10 @@ from django.db import models
 class Pedido(models.Model):
     nombre = models.CharField (("nombre pedido:"),max_length=25)
     descripcion = models.CharField (("descripcion pedido"),max_length=25)
-    reseta = models.CharField (("reseta pedido"),max_length=25)
+    receta = models.CharField (("receta pedido"),max_length=25)
 
-class reseta(models.Model):
-    analisissolicitado = models.CharField (("analisis reseta"),max_length=25)
+class receta(models.Model):
+    analisissolicitado = models.CharField (("analisis receta"),max_length=25)
     medicamentosolicitado = models.CharField (max_length=25)
     nombremedico = models.CharField (max_length=25)
     fecha =  models.DateField (max_length=25)
@@ -16,14 +16,24 @@ class mediciones(models.Model):
     nombremedicamento = models.CharField(max_length=25)
     cantML = models.CharField(max_length=25)
 
+class TipoDocumento(models.Model):
+    nombreTipoDocumento = models.CharField("Tipo de Documento", max_length=100)
+    descripcionTipoDocumento = models.CharField("Descripción del Tipo de Documento", max_length=200)
+
+    def __str__(self) -> str:
+        return self.nombreTipoDocumento
+    
 class usuario(models.Model):
     nombre = models.CharField(max_length=25)
     descripcion = models.CharField(max_length=25)
-    documento = models.CharField(max_length=25)
+    tipodocumento = models.ForeignKey(TipoDocumento, on_delete=models.CASCADE)
+    documento = models.IntegerField(max_length=25)
     direccion = models.CharField(max_length=25)
     telefono = models.CharField(max_length=25)
     email = models.EmailField(max_length=50)
 
+
+    
 class resultadopedidomedico(models.Model):
     nombre = models.CharField(max_length=25)
     descripcion = models.CharField(max_length=25)
