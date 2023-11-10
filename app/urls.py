a5bf3c7f0b80aa67749a47f7315bb83ce70a4eb6
JoaquinP <128108993+JoaKox5@users.noreply.github.com>
2023-11-10 02:32:07 -0300

    urls y views

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path("", home, name="home"),
-    path("login/", login, name="login"),
-    path("solicitud/", solicitud, name="solicitud")
+    path("login/", LoginView.as_view(), name="login"),
+    path("solicitud/", solicitud, name="solicitud"),
+    path("medico/", medico, name="medico"),
+    path("registropaciente/", registropaciente, name="registropaciente")
 ]
+
